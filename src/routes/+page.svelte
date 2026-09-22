2c7dddb87140d2f2eab94e60be1adeafb7bd5780
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-09-23 01:34:30 +0800

    Refactor: Update layout wrapper and implement loading states Improve page with fetching state handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,20 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import type { Todos } from '../types/Todo';
 	import TodoCard from '../components/todo.svelte';
 
 	let count = $state(0);
-	let todos: Todos = $state([]);
 
 	const handleClick = () => {
 		alert(`Button clicked ${++count} times!`);
 	};
 
 	const fetchTodos = async () => {
-		try {
-			const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-			todos = await response.json();
-		} catch (error) {
-			console.error('Error fetching todos:', error);
-			todos = [];
+		await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
+		const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
 		}
+		return await response.json();
 	};
-
-	onMount(async () => {
-		await fetchTodos();
-	});
 </script>
 
 <nav class="flex items-center justify-between bg-base-100 p-4">
@@ -33,8 +22,16 @@
 	<button class="btn btn-primary" onclick={handleClick}>Click Me!</button>
 </nav>
 
-<div class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 p-4">
-	{#each todos as todo}
-		<TodoCard {todo} />
-	{/each}
-</div>
+{#await fetchTodos()}
+	<div class="mt-6 grid place-items-center">
+		<div class="loading loading-spinner text-4xl text-primary"></div>
+	</div>
+{:then todos}
+	<div class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 p-4">
+		{#each todos as todo (todo.id)}
+			<TodoCard {todo} />
+		{/each}
+	</div>
+{:catch error}
+	<div class="m-4 alert alert-outline alert-error">Opps! Something went wrong!</div>
+{/await}
